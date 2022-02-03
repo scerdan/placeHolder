a6c406d82931d1761251c2efc1f19428527daa4d
Santiago Joel Cerdán <santyjoell@gmail.com>
2022-02-03 19:39:18 -0300

    Add Card

--- app/src/main/java/com/example/placeholder/ui/theme/InitSearchView.kt
+++ app/src/main/java/com/example/placeholder/ui/theme/InitSearchView.kt
@@ -1,9 +1,13 @@
 package com.example.placeholder.ui.theme
 
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.placeholder.models.Albums
 import com.example.placeholder.viewmodel.AlbumViewModel
@@ -56,13 +61,37 @@ fun MostrarListado(album: Albums) {
     Column() {
         LazyColumn() {
             items(album) {
-                Row(
-                    Modifier.fillMaxWidth(1f)
-                    .padding(3.dp),
-                    horizontalArrangement = Arrangement.Center, ) {
-                    Text(text = it.title,
-                        maxLines = 1,
-                    )
+                Card(
+                    shape = MaterialTheme.shapes.small, modifier = Modifier
+                        .fillMaxWidth(1f)
+                        .padding(10.dp)
+                        .clickable { },
+                    elevation = 5.dp
+                ) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth(1f)
+                            .padding(3.dp),
+                        horizontalArrangement = Arrangement.Start,
+                    ) {
+                        Text(
+                            text = it.id.toString(),
+                            maxLines = 1,
+                            fontSize = 10.sp,
+                        )
+                    }
+                    Row(
+                        Modifier
+                            .fillMaxWidth(1f)
+                            .padding(3.dp),
+                        horizontalArrangement = Arrangement.Start,
+                    ) {
+                        Text(
+                            text = it.title,
+                            maxLines = 1,
+                            fontSize = 30.sp
+                        )
+                    }
                 }
             }
         }
