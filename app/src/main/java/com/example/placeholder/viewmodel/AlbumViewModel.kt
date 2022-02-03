1d4a5c28314b9336a503db34fe8e75d46497dc50
Santiago Joel Cerdán <santyjoell@gmail.com>
2022-02-03 18:50:09 -0300

    Capture value textfield

--- app/src/main/java/com/example/placeholder/viewmodel/AlbumViewModel.kt
+++ app/src/main/java/com/example/placeholder/viewmodel/AlbumViewModel.kt
@@ -1,23 +1,22 @@
 package com.example.placeholder.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.placeholder.models.Albums
 import com.example.placeholder.models.AlbumsItem
 import com.example.placeholder.repository.AlbumRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AlbumViewModel @Inject constructor(private val repo: AlbumRepo) : ViewModel() {
-    private val _response = MutableLiveData<ArrayList<AlbumsItem>>()
+    private val _response = MutableLiveData<Albums>()
 
-    fun getUserFromApi(query: String): LiveData<ArrayList<AlbumsItem>> {
+    fun getUserFromApi(query: String): LiveData<Albums> {
         viewModelScope.launch(Dispatchers.IO) {
             val album = repo.getSearch(query)
             _response.postValue(album.body())
